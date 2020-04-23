a6677fae9f364cf91e8225a484f074282fc806e5
Ahmet <ahmetalperwork@gmail.com>
2020-04-23 14:38:59 +0200

    Add ability to post new secrets if registered/logged in

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -110,8 +111,25 @@ app.get('/register', function (req, res) {
 });
 
 app.get('/secrets', function (req, res) {
+
     if (req.isAuthenticated()) {
-        res.render('secrets');
+        User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUsers) {
+                    res.render('secrets', {usersWithSecrets: foundUsers})
+                }
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/submit', function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render('submit');
     } else {
         res.redirect('/login');
     }
@@ -152,6 +170,25 @@ app.post('/login', function(req, res) {
         }
     });
 });
+
+app.post('/submit', function(req, res) {
+
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect('/secrets');
+                });
+            }
+        }
+    })
+
+})
 
 
 app.listen(3000, function () {
